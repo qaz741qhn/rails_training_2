8fec3270febe7e41c19547e92612eed8d998b910
Lee Ching <qaz741qhn@gmail.com>
2021-08-27 15:58:24 +0900

    remove unused features

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,6 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-  def remember
-    self.remember_token = User.new_token
-    update_attribute(:remember_digest, User.digest(remember_token))
-  end
-
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
