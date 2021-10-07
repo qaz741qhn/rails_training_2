075d04ef413cc3f35c0b2a04119ba3cabe97ed7f
Lee Ching <qaz741qhn@gmail.com>
2021-10-07 19:29:08 +0900

    add views for diary

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,24 +1,26 @@
 class DiariesController < ApplicationController
 
-  before_action :logged_in?, only: [:show]
-  before_action :set_diary, only:[:show, :edit, :update]
+  before_action :logged_in?, only: [:new, :show, :edit]
+  before_action :set_diary, only:[:show, :edit, :update, :destroy]
+  before_action :auth_user, only: [:show]
 
   def index
     @diaries = Diary.all
   end
 
   def new
-    @diary = Diary.new
+    @diary = current_user.diaries.build
   end
 
   def show
+    @user = User.find(@diary.user_id)
   end
 
   def create
-    @diary = Diary.new(diary_params)
+    @diary = current_user.diaries.build(diary_params)
     if @diary.save
       flash[:notice] = "Diary '#{@diary.title}' is created successfully"
-      redirect_to(diary_path)
+      redirect_to(diary_path(@diary))
     else
       flash.now[:alert] = "Diary is not saved"
       render('new')
@@ -39,10 +41,13 @@ class DiariesController < ApplicationController
   end
 
   def destroy
-    user = User.find_by(email: params[:session][:email])
-    diary = user&.diary
-    diary.destroy if user == current_user
-    redirect_to(diaries_path)
+    user = User.find(@diary.user_id)
+    if user == current_user
+      @diary.destroy
+      redirect_to(diaries_path)
+    else
+      flash.now[:alert] = "You can only delete your own diary"
+    end
   end
 
   private
@@ -52,7 +57,20 @@ class DiariesController < ApplicationController
   end
 
   def diary_params
-    params.require(:diary).permit(:title, :article, :date)
+    params.require(:diary).permit(:title, :article, :date, :user_id)
+  end
+
+  def auth_user
+    if current_user
+      diary = Diary.find(params[:id])
+      user = User.find(diary.user_id)
+      unless user.id.to_s == current_user.id.to_s
+        flash[:alert] = "You can only access your own diary"
+        redirect_to(diaries_path)
+      end
+    else
+      redirect_to(login_path)
+    end
   end
 
 end
