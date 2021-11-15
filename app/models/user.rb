eb96a7b2a99604798c30a799ccd8f0e65ac08479
qaz741qhn <qaz741qhn@gmail.com>
2021-11-15 17:25:44 +0900

    fix code

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   def login_streak_count!
     if self.session.updated_at >= Time.current - 1.day
       self.login_streak += 1
-    elsif self.session.updated_at <= Time.current - 2.days.ago || self.session.updated_at.to_date == self.session.created_at.to_date
+    elsif self.session.updated_at < Time.current - 1.day.ago || self.session.updated_at.to_date == self.session.created_at.to_date
       self.login_streak = 1
     end
     self.save
