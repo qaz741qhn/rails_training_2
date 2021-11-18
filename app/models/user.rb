104ff9bf7fb20cc1616fab8fe293bcce69db7502
qaz741qhn <qaz741qhn@gmail.com>
2021-11-18 10:36:59 +0900

    fix logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,18 @@ class User < ApplicationRecord
   has_secure_password
 
   def login_streak_count!
-    if self.updated_at.between?(1.day.ago.beginning_of_day, 1.day.ago.end_of_day)
-      self.login_streak += 1
-    elsif self.updated_at <= 1.day.ago.end_of_day || self.updated_at.to_date == self.created_at.to_date
+    if self.updated_at <= 2.days.ago.end_of_day || self.updated_at.to_date == self.created_at.to_date
       self.login_streak = 1
+    elsif self.updated_at.between?(1.day.ago.beginning_of_day, 1.day.ago.end_of_day)
+      self.login_streak += 1
     end
     self.save
   end
 
   def daily_login_count!
-    if self.updated_at.between?(Time.current.beginning_of_month, 1.day.ago.end_of_day)
+    if self.updated_at <= Time.current.prev_month.end_of_month || self.updated_at.to_date == self.created_at.to_date
+      self.daily_logins = 1
+    elsif self.updated_at.between?(Time.current.beginning_of_month, 1.day.ago.end_of_day)
       self.daily_logins += 1
     end
     self.save
--- spec/model/session_spec.rb
+++ spec/model/session_spec.rb
@@ -14,14 +14,57 @@ RSpec.describe 'Session', type: :model do
 
     context 'login streak' do
       before do
-        user.created_at = 1.day.ago
+        user.created_at = 2.days.ago
         user.login_streak = 1
-        user.updated_at = Time.current
+        user.updated_at = 1.day.ago
         user.login_streak_count!
       end
       it 'streak increase' do
         expect(user.login_streak).to eq 2
       end
+
+      before do
+        user.created_at = 3.days.ago
+        user.login_streak = 1
+        user.updated_at = 2.days.ago
+        user.login_streak_count!
+      end
+      it 'streak equals 1' do
+        expect(user.login_streak).to eq 1
+      end
+
+      before do
+        user.created_at = Date.today
+        user.login_streak = 1
+        user.updated_at = Time.current
+        user.login_streak_count!
+      end
+      it 'streak equals 1' do
+        expect(user.login_streak).to eq 1
+      end
+    end
+
+    context 'monthly logins' do
+      before do
+        user.created_at = Time.current.beginning_of_month
+        user.daily_logins = 1
+        user.updated_at = 1.day.ago
+        user.daily_login_count!
+      end
+      it 'logins increase' do
+        expect(user.daily_logins).to eq 2
+      end
+
+      before do
+        user.created_at = Time.current.prev_month
+        user.daily_logins = 1
+        user.updated_at = Time.current.prev_month
+        user.daily_login_count!
+      end
+      it 'logins equals 1' do
+        expect(user.daily_logins).to eq 1
+      end
     end
 
   end
+end
